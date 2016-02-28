fd0acf88acbe806411cb9b59188501cf5d9524a2
Ionică Bizău <bizauionica@gmail.com>
2016-02-28 17:28:02 +0200

    :rocket:

--- example/index.js
+++ example/index.js
@@ -2,4 +2,28 @@
 
 const gmTools = require("../lib");
 
-console.log(gmTools());
+let img = gmTools(__dirname + "/octocat.jpg");
+
+// Get all the pixels
+img.pixels((err, pixels) => {
+    console.log(err || pixels);
+    // [ PixelClass { r: 34, g: 30, b: 31, a: 2.55 },
+    //   PixelClass { r: 34, g: 30, b: 31, a: 2.55 },
+    //   PixelClass { r: 34, g: 30, b: 31, a: 2.55 },
+    //   ...
+    //   PixelClass { r: 241, g: 241, b: 243, a: 2.55 },
+    //   ...
+    //   PixelClass { r: 191, g: 189, b: 190, a: 2.55 },
+    //   PixelClass { r: 195, g: 191, b: 192, a: 2.55 },
+    //   PixelClass { r: 92, g: 90, b: 91, a: 2.55 },
+    //   PixelClass { r: 32, g: 30, b: 31, a: 2.55 },
+    //   PixelClass { r: 32, g: 30, b: 31, a: 2.55 },
+    //   ...
+    //   PixelClass { r: 34, g: 30, b: 31, a: 2.55 } ]
+});
+
+// Get pixel at given coordinates
+img.getPixel(0, 0, (err, pixel) => {
+    console.log(err || pixel);
+    // PixelClass { r: 34, g: 30, b: 31, a: 2.55 }
+});
